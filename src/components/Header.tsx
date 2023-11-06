ec90f74e6aa32b91388eb813465231f89511a461
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-11-06 16:39:48 +0100

    working on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,34 @@
+'use client';
+
+import Select from 'react-select';
+import { BsHeart, BsCart } from 'react-icons/bs';
+import { AiOutlineUser } from 'react-icons/ai';
+
 const Header = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
   return (
     <nav>
-      <div>Logo</div>
-      <div>Category</div>
-      <div>Menu Icon</div>
+      <main>
+        <div>
+          Hollyway <span>global</span>
+        </div>
+
+        <div className='relative'>
+          <Select options={options} />
+          <input
+            type='search'
+            placeholder="i'm searching for..."
+            className='w-full '
+          />
+        </div>
+        <div>Menu Icon</div>
+      </main>
+      <hr />
     </nav>
   );
 };
