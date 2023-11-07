3e1e5c0c13da2ded05d7d07a982d769ab54b1ccb
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-11-07 03:22:04 +0100

    working cart section

--- src/components/PagesHeader.tsx
+++ src/components/PagesHeader.tsx
@@ -1,7 +1,15 @@
+'use client';
+import { RxHamburgerMenu } from 'react-icons/rx';
+
 const PagesHeader = () => {
   return (
-    <div className='flex justify-between items-center w-full px-[calc(1vw_+_1rem)] py-[calc(1vh_+_2rem)]'>
-      Pages Header
+    <div className='flex justify-between items-center w-full px-[calc(1vw_+_1rem)] py-[calc(1vh_+_1rem)]'>
+      <button className='flex justify-between items-center gap-4 bg-yellow-500 px-[2rem] py-2 rounded-md'>
+        <span className='text-[2rem]'>
+          <RxHamburgerMenu />
+        </span>
+        <h2>SHOP BY CATEGORY</h2>
+      </button>
     </div>
   );
 };
