d8b48ba2c3e8baf0adb5ad5f65d9eb2883a62eeb
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-11-07 05:25:11 +0100

    working on Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,51 @@
+"use client"
+
+import Slider from 'react-slick';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 4000,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+  };
+
+  return (
+    <div className="bg-[url('../assets/images/hero.jpg')] bg-no-repeat bg-cover bg-center">
+      <section className='flex justify-between  w-full px-[calc(1vw_+_1rem)] py-[calc(1vh_+_2rem)] gap-8'>
+        <article className='w-[70%] relative h-[25rem]'>
+          
+            <Slider {...settings}>
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+              <div>
+                <h3>6</h3>
+              </div>
+            </Slider>
+      
+        </article>
+
+        <article className=' bg-yellow-500 w-full'>Hello 2</article>
+      </section>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/PagesHeader.tsx
+++ src/components/PagesHeader.tsx
@@ -6,7 +6,7 @@ import { Nav_pages } from 'src/data/data';
 
 const PagesHeader = () => {
   return (
-    <div className='flex gap-8 items-center w-full px-[calc(1vw_+_1rem)] py-[calc(1vh_+_1rem)]'>
+    <div className='flex gap-8 items-center w-full px-[calc(1vw_+_1rem)] py-[calc(1vh_+_2rem)]'>
       <button className='flex justify-between items-center gap-4 bg-yellow-500 px-[2rem] py-2 rounded-md'>
         <span className='text-[2rem]'>
           <RxHamburgerMenu />
