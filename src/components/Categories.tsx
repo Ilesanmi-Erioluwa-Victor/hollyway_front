8a74b38392f6851fceff7efe46f6951021145713
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-11-08 14:12:21 +0100

    working on categories

---
+++ src/components/Categories.tsx
@@ -0,0 +1,9 @@
+const Categories = () => {
+  return (
+    <section className='pt-[4.8125rem] pr-[0.3125rem] pb-[4.6875rem] pl-[0.3125rem]'>
+      hello
+    </section>
+  );
+};
+
+export default Categories;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as PagesHeader } from './PagesHeader';
 export { default as Hero } from './Hero';
+export { default as Categories } from './Categories';
