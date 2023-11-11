d63eadb1627ce1b878d05c41c550a0b39259b2bf
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-11-11 09:34:39 +0100

    working on article

--- src/components/middle_section/Articles.tsx
+++ src/components/middle_section/Articles.tsx
@@ -2,6 +2,7 @@
 const Articles = () => {
   return (
     <article className="mt-[5.9375rem] mb-0 w-full relative">
+      <div className="max-w-[104.375rem] flex relative mx-auto"></div>
       <h2>Articles</h2>
     </article>
   )
