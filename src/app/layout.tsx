bd0c242301386a17865810a3b0796fe60f0fdb9f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-11-25 21:54:04 +0100

    working on responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-[#fff]`}>
         <>
           {children}
-          {/* <Footer /> */}
+          <Footer />
         </>
       </body>
     </html>
